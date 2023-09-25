0b984982845309dcf0e642b3119cf47c0cab30df
Faheem6765 <codingalt7@gmail.com>
2023-09-25 23:59:54 +0500

    port issue fixed

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const path = require("path");
 const UserModel = require("./Models/UserModel");
 const PhaseNotificationModel = require("./Models/PhaseNotification");
 const { sendNotification } = require("./Utils/sendNotification");
-const {initializeSocket} = require("./socket")
+// const {initializeSocket} = require("./socket")
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/Uploads", express.static("Uploads"));
+app.use(cookieParser());
 
 app.use(
   cors({
@@ -33,8 +33,8 @@ app.use((req,res,next)=>{
     "Access-Control-Allow-Origin",
     "https://immigrationmatter.netlify.app"
   );
-  res.setHeader("Access-Control-Allow-Credentials: true");
-  res.setHeader("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+  // res.setHeader("Access-Control-Allow-Credentials: true");
+  // res.setHeader("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
   next();
 })
 
@@ -48,6 +48,75 @@ app.use(require("./Routes/PhaseNotificationRoute"));
 
 const server = app.listen(PORT, () => {});
 // const io = initializeSocket(server);
+
+// Socket IO Code Starts
+  const io = require("socket.io")(server, {
+    pingTimeout: 60000,
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Connected to socket.io");
+
+    socket.on("setup", (user) => {
+      socket.join(user?.userId);
+      socket.emit("connected");
+    });
+
+    socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User Joined room", room);
+    });
+
+    socket.on("new message", (chat) => {
+      if (!chat?.users) return console.log("chat.users is undefined");
+
+      chat.users.forEach((user) => {
+        if (user == chat.sender) return;
+        socket.in(user).emit("message received", chat);
+      });
+    });
+
+    socket.off("setup", () => {
+      console.log("User Disconnected");
+      socket.leave(user.userId);
+    });
+
+    // Send Notification Of Client to Admin for Phase Approval
+    socket.on("send phase data", async (request) => {
+      console.log("send phase data", request);
+      if (!request) return console.log("Phase Data Request is undefined");
+      const admins = await UserModel.find({ isAdmin: true });
+
+      // Send Notification and Store Request In Database
+      sendNotification({
+        title: "Phase Data Request",
+        userId: request.userId,
+        applicationId: request.applicationId,
+      });
+      admins.forEach((admin) => {
+        socket.in(admin._id.toString()).emit("phase data received", request);
+      });
+    });
+
+    // Send Notification to Client of phase Approval from Admin
+    socket.on("phase notification", async (request) => {
+      console.log("Phase Notification", request);
+      if (!request) return console.log("Phase Notification is undefined");
+
+      // Send Notification and Store Request In Database
+      sendNotification({
+        title: request.title,
+        userId: request.userId,
+        applicationId: request.applicationId,
+      });
+        socket.in(request.userId).emit("phase notification received", request);
+      
+    });
+
+  });
 
 app.get("/", (req, res) => {
   res.send("Api is running successfully");
