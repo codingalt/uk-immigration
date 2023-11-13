fb810ad851f9d871bd96f1c8898fb4aca9fb5013
Faheem6765 <codingalt7@gmail.com>
2023-11-13 10:04:25 +0500

    fixing email issues

--- Routes/ChatRoute.js
+++ Routes/ChatRoute.js
@@ -20,7 +20,7 @@ const upload = multer({ storage: storage });
 router.post("/api/chat", Authenticate, accessChat);
 router.get("/api/chats", Authenticate, getUserChats);
 router.get("/api/chat/:applicationId", Authenticate, getChatByApplicationId);
-router.post("/api/message", Authenticate,upload.array('chatFile',5), sendMessage);
+router.post("/api/message", Authenticate,upload.array('chatFile',5), isAssignedCaseWorker, sendMessage);
 router.get("/api/message/:chatId", Authenticate, getAllMessages);
 
 module.exports = router;
